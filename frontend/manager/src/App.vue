0e647bee0c29063cf2f24f72682b6036f4c78a5a
MagnusSeverin <magnusshp@gmail.com>
2022-02-20 16:38:26 +0100

    Created a folder for frontend part
    Initial commit

---
+++ frontend/manager/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Hospital Management</h1>
+    <h1>Test</h1>
+    <h3>Functions</h3>
+      <ol>
+        <li>List of hospitals</li>
+        <li>Add new hospital</li>
+      </ol>
+  </div>
+  <div>
+    <b-button>Button</b-button>
+    <b-button variant="danger">Button</b-button>
+    <b-button variant="success">Button</b-button>
+    <b-button variant="outline-primary">Button</b-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+  * {
+    font-family: 'Times New Roman', Times, serif;
+  }
+</style>
