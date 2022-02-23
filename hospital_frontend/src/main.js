85cf18818c01535ef23abebca21d822ef0e04378
aperinus <s193939@student.dtu.dk>
2022-02-23 15:19:08 +0100

    node_modules
    Forsøg på manuelt at tilføje mappe

--- hospital_frontend/src/main.js
+++ hospital_frontend/src/main.js
@@ -4,6 +4,14 @@ import axios from 'axios'
 import App from './App.vue'
 import Backend from './services/backend'
 import router from './router'
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+import { Vue } from 'vue/types/vue'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 const app = createApp(App)
 
@@ -13,3 +21,5 @@ app.config.globalProperties.axios = axios
 app.config.globalProperties.$backend = new Backend()
 
 app.mount('#app')
+
+
