2d7ef6a581c7ec13f056d9c2bd9a67f60ed980fe
aperinus <s193939@student.dtu.dk>
2022-02-25 08:49:05 +0100

    Removal of BootstrapVue imports

--- hospital_frontend/src/App.vue
+++ hospital_frontend/src/App.vue
@@ -13,12 +13,6 @@
         @new-hospital="refresh"
         @hospital-deleted="refresh" />
   </div>
-  <div>
-    <b-button>Button</b-button>
-    <b-button variant="danger">Button</b-button>
-    <b-button variant="success">Button</b-button>
-    <b-button variant="outline-primary">Button</b-button>
-  </div>
 </template>
 
 <script>
