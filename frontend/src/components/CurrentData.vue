6224747be00fc6961614b2befc7e975c4a16ea2f
aperinus <s193939@student.dtu.dk>
2022-06-15 14:07:10 +0200

    CurrentData component created

---
+++ frontend/src/components/CurrentData.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+    <div id="name"> {{this.observation[0]}}: </div>
+    <span id="data"> {{this.observation[1]}} </span>
+    <span id="time"> {{this.observation[2]}} </span>
+    <div> øhhhh </div>
+</div>
+</template>
+
+
+<script>
+export default {
+  name: 'CurrentData',
+  props: 'observation',
+  data(){
+  },
+  methods:{
+  }
+}
+</script>
+
+
+<style>
+
+div#name {
+    float: left;
+    width: 10vw;
+}
+span#data {
+    background-color: whitesmoke;
+    padding: 2px;
+    width: 2vw;
+}
+span#time{
+    color: red;
+    font-size: 0.7em;
+    padding-left: 10px;
+}
+
+</style>
